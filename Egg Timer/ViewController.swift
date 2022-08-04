ca2a7298208021c334063c4fce0d7f39cc130fd6
Aditya Virbhadra Vyavahare <vvaditya23@icloud.com>
2022-08-04 13:23:39 +0530

    Initial Commit

---
+++ Egg Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Egg Timer
+//
+//  Created by Aditya Virbhadra Vyavahare on 04/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
