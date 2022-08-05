c203208499ce01255c02304ee63868177a6ede1a
Aditya Virbhadra Vyavahare <vvaditya23@icloud.com>
2022-08-05 23:00:28 +0530

    Created eggTime dict, added timer (needs to be calibrated).

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /*let softTime = 2
+    let mediumTime = 3
+    let hardTime = 4*/
+    let eggTimes: [String:Int] = ["Soft": 2, "Medium": 3, "Hard": 4]
+    
+    var hardness: String = "null"
+    var counter = 10
+    var timer = Timer()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+            
+        timer.invalidate()
+        hardness = sender.title(for: .normal)!
+        //print(eggTimes[hardness]!)
 
+        // start the timer
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+    }
+    
+    // called every time interval from the timer
+    @objc func timerAction() {
+        counter -= 1
+        if(counter == 0) {
+            //counter = 0
+            timer.invalidate()
+        }
+        print("\(counter)")
+    }
+    
 }
 
