dde4c7ee469a13bcc878c41877855e21c9b81df3
Aditya Vyavahare <vvaditya23@icloud.com>
2022-08-07 17:54:16 +0530

    Completed!

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -9,15 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var OnScreenTextLabel: UILabel!
+    @IBOutlet weak var progressBarView: UIProgressView!
+    
     /*let softTime = 2
     let mediumTime = 3
     let hardTime = 4*/
-    let eggTimes: [String:Int] = ["Soft": 2, "Medium": 3, "Hard": 4]
+    let eggTimes: [String:Int] = ["Soft": 10, "Medium": 15, "Hard": 20]     //dictionary
     
     var hardness: String = "null"
-    var counter = 10
+    var counter = 0
     var timer = Timer()
-
+    var totalTime = 0
+    var progressPercentage: Float = 1.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,8 +31,11 @@ class ViewController: UIViewController {
     @IBAction func hardnessSelected(_ sender: UIButton) {
             
         timer.invalidate()
+        progressBarView.progress = 0.0
+        progressPercentage = 0
+        
         hardness = sender.title(for: .normal)!
-        //print(eggTimes[hardness]!)
+        totalTime = eggTimes[hardness]!
 
         // start the timer
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
@@ -36,13 +43,21 @@ class ViewController: UIViewController {
     
     // called every time interval from the timer
     @objc func timerAction() {
-        counter -= 1
-        if(counter == 0) {
-            //counter = 0
+        if(counter < totalTime) {
+            OnScreenTextLabel.text = "Egg's are being boiled to be '\(hardness) cored'!                           wait & wait & wait..."
+            print("\(counter)")
+            counter += 1
+            
+            progressPercentage = Float(counter)/Float(totalTime)
+            print(progressPercentage)
+            progressBarView.progress = progressPercentage
+        } else {
+            OnScreenTextLabel.text = "Your '\(hardness) cored' egg's are ready!"
+            progressPercentage = 0
+            totalTime = 0
+            counter = 0
             timer.invalidate()
         }
-        print("\(counter)")
     }
-    
 }
 
